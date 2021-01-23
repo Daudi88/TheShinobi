b876c2358e6304a8c03ab23cf6ac72c9a42df710
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-24 00:29:17 +0100

    Enemy Haku skapad.

---
+++ TheShinobi/Characters/Enemies/Haku.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Characters.Enemies
+{
+    class Haku : Character
+    {
+        public Haku()
+        {
+            Name = "Haku";
+            Level = 4;
+            Hp = 50;
+            Exp = 50;
+            Gold = Utility.RollDice(100 * Level);
+            Armor = new FlackJacket();
+            Defence = Armor.Defence;
+            Weapon = new Crossbow
+        }
+    }
+}
