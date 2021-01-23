c9bb62f2d716a4285936827592b6adc8d722db48
David Ström <david.strom88@gmail.com>
2021-01-23 23:10:03 +0100

    Testade en merge från Daudi.

---
+++ TheShinobi/Characters/Player.cs
@@ -0,0 +1,6 @@
+﻿namespace TheShinobi.Characters
+{
+    class Player : Character
+    {
+    }
+}
---
+++ TheShinobi/Interfaces/IEquipable.cs
@@ -0,0 +1,10 @@
+﻿using TheShinobi.Characters;
+
+namespace TheShinobi.Interfaces
+{
+    interface IEquipable
+    {
+        public string Name { get; set; }
+        public void Equip(Player player, IEquipable item);
+    }
+}
