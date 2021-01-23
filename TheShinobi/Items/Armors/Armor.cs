f26f0930c6a57788e195b648bc7d877eafbf38bf
David Ström <david.strom88@gmail.com>
2021-01-24 00:12:32 +0100

    Lagt till Armors, Weapons, Potions m.m.

---
+++ TheShinobi/Items/Armors/Armor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TheShinobi.Characters;
+using TheShinobi.Interfaces;
+
+namespace TheShinobi.Items.Armors
+{
+    abstract class Armor : Item, IEquipable
+    {
+        public int Defence { get; set; }
+
+        public void Equip(Player player, IEquipable item)
+        {
+            if (player.Armor != null)
+            {
+                player.Backpack.Add(player.Armor);
+            }
+            Console.WriteLine($"\t You equipped {Item.Name} with {Defence} defence!");
+            player.Armor = (Armor)item;
+            player.Defence = Defence;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Defence} Defence)";
+        }
+    }
+}
---
+++ TheShinobi/Items/Potions/Potion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TheShinobi.Characters;
+
+namespace TheShinobi.Items.Potions
+{
+    class Potion : Item
+    {
+        public int Health { get; set; }
+        public string Text { get; set; }
+        public void Consume(Player player)
+        {
+            Console.WriteLine($"\t {Text}");
+            if (player.Hp < player.MaxHp)
+            {
+                player.Hp += Health;
+                if (player.Hp >= player.MaxHp)
+                {
+                    player.Hp = player.MaxHp;
+                    Console.WriteLine("\t You gained full health!");
+                }
+                else
+                {
+                    Console.WriteLine($"\t You gained {Health} health");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Quantity} {Name} (+{Health} Hp)";
+        }
+    }
+}
