43a8d85677e358d9f25bd5f70ce4bd0e518ddd80
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-24 00:03:06 +0100

    Lade till klassen Player.

---
+++ TheShinobi/Characters/Enemies/Ginkaku.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Characters.Enemies
+{
+    class Ginkaku : Character
+    {
+        public Ginkaku() // "Ginkaku", 2, 20, 6, "2d4", 100
+        {
+            
+        }
+        
+        
+    }
+}
--- TheShinobi/Characters/Player.cs
+++ TheShinobi/Characters/Player.cs
@@ -2,5 +2,26 @@
 {
     class Player : Character
     {
+        public double Pos { get; set; } = 0.0;
+        public int MaxHp { get; set; }
+        public int MaxExp { get; set; }
+        public List<Item> Backpack { get; set; } = new List<Item>();
+        public Player()
+        {
+            MaxHp = MaxHp;
+            MaxExp = 200 * Level;
+            Weapon = new Fists();
+        }
+        public void LevelUp()
+        {
+            LevelUp++;
+            MaxExp += 2++ * LevelUp;
+            MaxHp += LevelUp;
+            if (MaxHp > 1000)
+            {
+                MaxHp = 1000;
+            }
+            MaxHp = MaxHp;
+        }
     }
 }
