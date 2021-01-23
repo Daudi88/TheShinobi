16a9d906688fbd1005a452effca539f47d6db8e2
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-23 23:28:39 +0100

    Övar merge lade till Deidara klassen.

---
+++ TheShinobi/Enemies/Deidara.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheShinobi.Characters;
+
+namespace TheShinobi.Enemies
+{
+    class Deidara : Enemy
+    {
+        public override void DropItems(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
