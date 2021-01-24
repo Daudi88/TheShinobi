879a6d02ae89e0fa20a8a43242f2f7f7418c5c92
David Ström <david.strom88@gmail.com>
2021-01-24 11:00:28 +0100

    Lagt till TypeOverWrongDoings i Utility.

--- TheShinobi/HelperMethods/Utility.cs
+++ TheShinobi/HelperMethods/Utility.cs
@@ -1,10 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
 
 namespace TheShinobi.HelperMethods
 {
-    class Utility
+    static class Utility
     {
+        static readonly Random random = new Random();
+
+        public static int RollDice(string dice)
+        {
+            int times = int.Parse(dice[0].ToString());
+            int sides = int.Parse(dice[2..]);
+            int result = 0;
+            for (int i = 0; i < times; i++)
+            {
+                result += random.Next(1, sides + 1);
+            }
+            return result;
+        }
+
+        public static void TypeOverWrongDoings(string input, string message = null)
+        {
+            if (message == null)
+            {
+                message = "Invalid choice. Try again!";
+            }
+            ColorConsole.Write($"\t {message}", ConsoleColor.Red);
+            Thread.Sleep(1800);
+            int left = 9;
+            int top = Console.CursorTop;
+            Console.SetCursorPosition(left, top--);
+            for (int j = 0; j < message.Length; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.SetCursorPosition(left, top);
+            for (int i = 0; i < input.Length + 2; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.SetCursorPosition(left, top);
+            Console.Write("> ");
+        }
     }
 }
