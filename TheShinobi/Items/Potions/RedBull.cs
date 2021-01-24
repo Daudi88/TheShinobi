3218305fe369aa1b3e4f5ec3b0e0b0a3708b555f
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-24 02:06:55 +0100

    Potions HealingPotion samt RedBull tillagda.

---
+++ TheShinobi/Items/Potions/RedBull.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Items.Potions
+{
+    class RedBull : Potion
+    {
+        public RedBull()
+        {
+            Name = "Red Bull";
+            Cost = 50;
+            Healing = 20;
+            Text = "You drink a powerfull potion that gives you wings!";
+        }
+    }
+}
