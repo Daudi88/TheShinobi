019bbe6a93b36a05e0fa7343b0fe79f7b146a5c0
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-24 22:32:52 +0100

    Enemies tillagda och sorterade i Level ordning

--- TheShinobi/HelperMethods/Utility.cs
+++ TheShinobi/HelperMethods/Utility.cs
@@ -47,35 +47,47 @@ namespace TheShinobi.HelperMethods
         {
             Enemy[] enemies = new Enemy[]
             {
-                new Enemy("Hanzō", 7, 100, new FlakJacket(), new Kusarigama()),
-                new Enemy("Daudi", 9, 100, new BulletproofVest(), new AK47()),
-                new Enemy("Hocke", 9, 100, new BulletproofVest(), new AK47()),
-                new Enemy("Deidara", 7, 100, new FlakJacket(), new Gunbai()),
-                new Enemy("Ginkaku", 2, 100, new FlakJacket(), new Shichiseiken()),
-                new Enemy("Haku", 4, 100, new FlakJacket(), new Crossbow()),
-                new Enemy("Kabuto", 1, 100, new FlakJacket(), new ChakraBlade()),
-                new Enemy("KaguyaOtsutsuki", 2, 100, new FlakJacket(), new FistsOfBones()),
-                new Enemy("Kakuzu", 5, 100, new InfiniteArmor(), new Sword()),
-                new Enemy("Kimimaro", 3, 100, new FlakJacket(), new FistsOfBones()),
-                new Enemy("Kisame", 1, 100, new FlakJacket(), new Kunai()),
-                new Enemy("Madara", 1, 100, new FlakJacket(), new Gunbai()),
-                new Enemy("Nagato", 2, 100, new FlakJacket(), new Sword()),
-                new Enemy("Obito", 2, 100, new FlakJacket(), new Shichiseiken()),
-                new Enemy("Orochimaru", 5, 100, new ShinobiBattleArmor(), new Spear())
+                new Enemy("Sakon", 1, 30, new FlakJacket(), new Kiba()),
+                new Enemy("Tayuya", 1, 30, new Shirt(), new Fists()),
+                new Enemy("Ukon", 2, 32, new Shirt(), new Kusarigama()),
+                new Enemy("Sasori", 2, 32, new Shirt(), new TekagiShuko()),
+                new Enemy("Konan", 3, 35, new FlakJacket(), new Kubikiribōchō()),
+                new Enemy("Nagato", 3, 35, new SteamArmor(), new Sword()),
+                new Enemy("Haku", 4, 39, new FlakJacket(), new Crossbow()),
+                new Enemy("Obito", 4, 39, new ChakraArmor(), new Gunbai()),
+                new Enemy("Kaguya", 5, 44, new FlakJacket(), new FistsOfBones()),
+                new Enemy("Ginkaku", 5, 44, new SteamArmor(), new Shichiseiken()),
+                new Enemy("Madara", 6, 50, new InfiniteArmor(), new Gunbai()),
+                new Enemy("Hanzō", 6, 50, new InfiniteArmor(), new Kusarigama()),
+                new Enemy("Deidara", 7, 57, new SteamArmor(), new Shuriken()),
+                new Enemy("Kimimaro", 7, 100, new ChakraArmor(), new FistsOfBones()),
+                new Enemy("Kabuto", 8, 65, new SteamArmor(), new Shichiseiken()),
+                new Enemy("Kisame", 8, 65, new FlakJacket(), new Kunai()),
+                new Enemy("Daudi", 9, 74, new BulletproofVest(), new AK47()),
+                new Enemy("Hocke", 9, 74, new BulletproofVest(), new AK47()),
+                new Enemy("Kakuzu", 10, 84, new InfiniteArmor(), new Spear())
+               // new Enemy("Orochimaru", 10, 100, new ShinobiBattleArmor(), new ChakraBlade()) Lägg i bossfight
 
             };
             return enemies;
+        }
+        public static Weapon[] GetWeapons()
+        {
+            Weapon[] weapons = new Weapon[]
+            {
+                new Crossbow(),
+                new ChakraBlade(),
+            };
         }
 
         public static Potion[] GetPotions()
         {
             Potion[] potions = new Potion[]
             {
-                //new Potion("Lesser Healing Potion"),
-                //new Potion(),
-                //new Potion()
-                
-                new Potion("Lesser Healing Potion", , ,""),
+                new Potion("Lesser Healing Potion", 15, 5),
+                new Potion("Healing Potion", 20, 8),
+                new Potion("Greater Healing Potion", 25, 10),
+                new Potion("Superior Healing Potion", 30, 12)
             };
             return potions;
         }
--- TheShinobi/Items/Weapons/Weapon.cs
+++ TheShinobi/Items/Weapons/Weapon.cs
@@ -7,6 +7,7 @@ namespace TheShinobi.Items.Weapons
     class Weapon : Item, IEquipable
     {
         public string Damage { get; set; }
+        
 
         public void Equip(Player player, IEquipable item)
         {
