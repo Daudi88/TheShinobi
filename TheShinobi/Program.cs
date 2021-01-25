677fb3a0b7599fa515195d97707e9b11735c65c9
David Ström <david.strom88@gmail.com>
2021-01-25 21:40:24 +0100

    Lade till en TODO.

--- TheShinobi/Program.cs
+++ TheShinobi/Program.cs
@@ -9,7 +9,14 @@ namespace TheShinobi
             Game game = new Game();
             game.Setup();
             game.Start();
-
         }
+
+        // TODO
+        // Fixa implementeringen av Abu Hassans Shop. => AK-47, Bulletproof vest, Red Bull
+        // Fixa implementeringen Så man får något vid graveyard.
+        // Fixa implementeringen Så man får något vid Treasure.
+        // Fiender ska droppa items när de dör!
+        // Kan man köpa/sälja flera quantity på samma gång? Hur många vill du köpa? Bara om det är en consumable.    
+        // Backpack sell måste fixas
     }
 }
