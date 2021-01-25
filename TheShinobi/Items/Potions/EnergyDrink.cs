30c7c8d5bac2f16e200022c05f269ac72ddb0738
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-25 23:27:04 +0100

    EnergyDrink tillagd.

---
+++ TheShinobi/Items/Potions/EnergyDrink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Items.Potions
+{
+    class EnergyDrink : Consumable
+    {
+        public int Caffeine { get; set; }
+        public EnergyDrink(string name, int cost, int health, string text, int caffeine) : base(name, cost, health, text)
+        {
+            Caffeine = caffeine;
+        }
+    }
+}
