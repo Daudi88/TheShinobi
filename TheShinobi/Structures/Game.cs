73dd3e389c9f453aeaa446a6ab703be003d2d51c
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-28 22:40:20 +0100

    Intro text klar.

--- TheShinobi/Structures/Game.cs
+++ TheShinobi/Structures/Game.cs
@@ -45,14 +45,17 @@ namespace TheShinobi.Structures
             Adventure.villagePlayer.PlayLooping();
             Display.Title();
             Player player = CharacterCreation();
-            string intro = $"\n\t You {player.Name} wake up from a womans scream's and it sound's as if she is beeing taken away!!!" +
-            "\n\t You hear the villagers scream that somebody kidnapped Hanare! \n" +
-            "\n\t You gear up for battle! \n " +
-            "\t It is still dark outside as you prepare to leave The Hidden Leaf Village in search of Hanare" +
-            "\n\t and her soon to be dead kidnappers...\n" +
-            "\n\t You will probobly need a better Weapon and Armor, you have heard about treasures containing some usefull loot." +
-            "\n\t Make sure you are strong and equipped enough for when you meet Hanare's Kidnapper." +
-            "\n\t Take care and dont loose yourself in the wild...";
+            string intro = $"\n\t You {player.Name} wake up from a woman screaming! \n" +
+            "\t It sound's as if she is beeing killed!!! \n" +
+            "\n\t Frightened villagers scream for help “somebody is kidnapping Hanare!“" +
+            "\n\t After some intense fighting the attackers manages to vanish north towards the mountains!" +
+            "\n\t You couldnt reach them in time... \n" +
+            "\n\t It is still dark outside when you prepare to leave The Hidden Leaf Village" +
+            "\n\t in search of Hanare and her soon to be dead kidnappers...\n" +
+            "\n\t You will probobly need better Weapon's and Armor's! " +
+            "\n\t * There are rumors about treasures containing some usefull loot." +
+            "\n\t * Make sure you are strong and equipped enough for when you meet Hanare's Kidnapper." +
+            "\n\t * Take care and make sure you don't loose yourself in the wild...";
             ColorConsole.WriteDelayedLine(intro, ConsoleColor.Yellow, 40);
             Display.Blinking("\t [Press enter to continue]");
             //Console.WriteLine("\t [Press enter to continue]");
