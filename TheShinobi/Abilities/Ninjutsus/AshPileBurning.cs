604389cd303bcccbf90d26439be27096a8d6a5bc
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-29 14:17:33 +0100

    Lade till ytterligare 3 Ninjutsus.

---
+++ TheShinobi/Abilities/Ninjutsus/AshPileBurning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Abilities.Ninjutsus
+{
+    class AshPileBurning : Ninjutsu
+    {
+        public AshPileBurning()
+        {
+            Name = "AshPile Burning";
+            Damage = "2d10";
+            Cost = 20;
+        }
+    }
+}
