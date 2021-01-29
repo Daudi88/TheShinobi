ec79f292b976812fd3bd403aa1a2a7a846114b75
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-29 22:50:00 +0100

    Ändrat lite i fightstorys

--- TheShinobi/HelperMethods/Get.cs
+++ TheShinobi/HelperMethods/Get.cs
@@ -424,7 +424,7 @@ namespace TheShinobi.HelperMethods
                 $"\n\t  As you approach {enemy.Name} attack you!",
 
                 $"\t  Some enemies from {enemy.Clan} clan are blocking your path" +
-                $"\n\t  There is no time to loose Hanare must be found quickly, you attack! {enemy.Name}",
+                $"\n\t  There is no time to loose Hanare must be found quickly, you attack {enemy.Name}!",
 
                 $"\t  You sence there is trouble ahead! " +
                 $"nt  The {enemy.Clan} clan is trying to hide around the corner" +
