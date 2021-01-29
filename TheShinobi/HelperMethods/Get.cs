067ce93015338d00f5e2f0ff8341a7b17de3b793
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-29 11:54:06 +0100

    End story uppdaterad.

--- TheShinobi/HelperMethods/Get.cs
+++ TheShinobi/HelperMethods/Get.cs
@@ -315,7 +315,7 @@ namespace TheShinobi.HelperMethods
                 "\t  You walk down the bushy trail and sence there is trouble ahead!" +
                 $"\n\t  You can smell that dirty {enemy.Clan} member miles away and you attack instantly!",
 
-                $"\t  While climbing up the mountains in search of Hanare you get ambushed by some filthy {enemy.Clan} villains!" +
+                $"\t  While climbing up the mountains in search of Hanare you get ambushed by some filthyKLIPPHÄR {enemy.Clan} villains!" +
                 "\n\t  You attack, maybe they know more about Hanare's kidnapping!",
 
                 "\t  You have walked for hours when you decide to set up camp at a lake." +
@@ -419,7 +419,7 @@ namespace TheShinobi.HelperMethods
         public static string[] EndStory(Player player)
         {
             string[] stories = new string[]
-            {
+                {
                 "\t After fighting Orochimaru for hours you finally wear him down and kill him. \n" +
                 $"\t As you dry Orochimaru's blood from your weapon on his Flack jacket.",
 
@@ -430,83 +430,91 @@ namespace TheShinobi.HelperMethods
                 "\n\t As you walked further into the huge cave system Hanares cry for help \n" +
                 "\t get's clearer. You are closing in on her!",
 
-
-
-                "\t Suddenly you sense danger! and instinctivly make a Shinobi backflip! \n" +
+                "\t Suddenly you sense danger from behinde \n" +
+                "\t you instinctivly make a Shinobi backflip! \n" +
                 "\n\t You dodge the sword that was beeing aimed at your back with full force!" +
                 "\n\t You knew it! It is the filty backstabbing Akatsuku Clan! \n" +
                 "\n\t Nobody else would ever try to kill a Shinobi from behinde." +
                 "\n\t Three Akatsuku are attacking and they have you cornered. \n",
 
                 "\t Beeing a true ninjutsu hero you are draw your Weapon and counter attack. \n" +
-                "\t The closest attacker throws a Kunai aimed at your heart! " +
-                "\n\t You use your chakra and redirect the knife into on of the other dirty Akatsu scumbag's! \n",
+                "\t The closest attacker throws a Kunai aimed at your heart! \n" +
+                "\n\t You use your chakra and redirect the knife into on of the other dirty" +
+                "\n\t Akatsu scumbag's! \n",
 
-                "\t This fight did not last longer then a few minutes. They where weak and in your way. \n" +
+                "\t This fight did not last longer then a few minutes. \n" +
+                "n\t They where weak and in your way. \n" +
                 "\t Now they are all very very dead...\n",
 
                 "\t You sense Hanare is close and you walk into one of the dark side tunnels...\n" +
                 "\t Your senses tells you that Hanare is not alone! \n" +
                 "\t Someone with the ability to hide his true powers are also present.\n\n",
 
-                "\t Who is this, and how can he block your senses? \n",
-
+                "\t Who is this, and how can he block your senses? \n" +
                 "\t You feel that the enemy and Hanare is just around the next corner. \n" +
-                "\t As you draw your {player.weapon} and walk around the corner you se Hanare and him, HIM...\n\n",
+                "\t As you draw your weapon and walk around the corner you se Hanare and him, HIM...\n\n",
 
-                "\t This is impossible... He is dead, you know you killed him and every last member of their Clan. \n" +
-                "\t this cant be!?! Kakuzu's alive? \n\n",
+                "\t This is impossible... He is dead!?! \n" +
+                "\t you know you killed him and every last member of their Clan. \n" +
+                "\t this cant be!?! Kakuzu's alive? \n",
 
-                "\t Kakuzu laughs like a mad man when he sees your confusion -“HA HA HA HA“ \n" +
-                "\t -“Did you not know that I teamed up with Orihime Inoue years ago? YOU FOOL!“ \n" +
-                "\t -“Orihime brought me back from the dead not more then 30 minutes after you left! \n" +
-                "\t -“She truly is special my dear Orihime...“",
+                "\t Kakuzu laughs like a mad man when he sees your confusion “HA HA HA HA“ \n" +
+                "\n\t “Did you not know that I teamed up with Orihime Inoue years ago? YOU FOOL!“ \n" +
+                "\t “Orihime brought me back from the dead not more then 30 minutes after you left! \n" +
+                "\t “She truly is special my dear Orihime...“",
 
-                "\t -“You should have killed her when you had the chance!“ \n" +
-                "\t -“Instead I will now kill someone near and dear to you! \n" +
-                "\t -“I want you to watch me kill Hanare before I come back and kill you to!” \n" +
-                "\t -“GRAB HIM!!!“\n\n",
+                "\t “You should have killed her when you had the chance!“ \n" +
+                "\n\t “Instead I will now kill someone near and dear to you! \n" +
+                "\t “I want you to watch me kill Hanare before I come back and kill you to!” \n" +
+                "\t “GRAB HIM!!!“\n\n",
 
-                "\t You are suddenly overwhelmed from behinde by enemies pulling you down towards the ground, \n" +
+                "\t You are suddenly overwhelmed by enemies pulling you down towards the ground, \n" +
                 "\t where did they come from!? \n" +
-                "\t Kakuzu must have used his {ability} to block my senses! \n",
+                "\t Kakuzu must have used his {ability} to block your senses! \n",
 
                 "\t Well, if this how he wants to play, fine by me! \n" +
-                "\t -“You shall meet your own powers you fool!“ \n\n" +
+                "\t “You shall meet your own powers you fool!“ \n\n" +
                 "\t You remove your forhead protector and use your sharingan! \n" +
                 "\n\t Your insight in Kakuzu's powers makes you block his senses \n" +
                 "\t and he has no idea that you are approaching him. \n\n " +
                 "\t Until its to late... \n\n",
 
-                "\t “The rage {players.name} unleashed upon Kakuzu has never been seen from any Shinobi \n" +
+                "\t “The rage {players.name} unleashed upon Kakuzu \n" +
+                "\t has never been seen from any Shinobi ever before.\n" +
                 "\t A true Shinobi are known to keep their cool even when angry! \n" +
                 "\t The tale of this fight will live on as a tale forever...“\n\n",
 
-                "\t As you come to your senses you rush to Hanare and untie the ropes around her hands and neck \n" +
+                "\t As you come back to your senses you rush to Hanare \n" +
+                "\t and untie the ropes around her hands and neck \n" +
                 "\t she is still unconscious and very very weak. \n " +
-                "\t without a second of rest you pick Hanare up and carry her all the way back to The Hidden Leaf Village.\n\n",
+                "\t without a second of rest you pick Hanare up and \n" +
+                "\t carry her all the way back to The Hidden Leaf Village.\n\n",
 
-                "\n\t As you approach the village, you scream for help.\n" +
-                "\t Tsunade meets you infront of Konoha Hospital and instantly brings Hanare in for care... \n " +
-                "\n\t You have waited hours for the medical-nin to heal Hanare when you suddenly hear Hanares voice.\n",
+                "\n\t As you approach the village, you call for help.\n" +
+                "\t Tsunade meets you infront of Konoha Hospital \n" +
+                "\t and instantly brings Hanare in for care... \n " +
+                "\n\t You have waited hours for the medical-nin to heal Hanare. \n" +
+                "\t Suddenly you hear Hanares voice...\n\n",
 
-                "\t -“Thank you {player.name}! You rescued me“ \n" +
-                "\t -“I knew you would find me, THANK YOU HERO!” \n",
+                "\t “Thank you {player.name}! You rescued me“ \n" +
+                "\t “I knew you would find me, THANK YOU HERO!” \n\n",
 
-                "\t The peace is back in The Hidden Leaf Village \n\n " +
-                "\t CREDITS: \n" +
+                "\t The peace is back in The Hidden Leaf Village \n " +
+                "\n\t THE END... \n\n",
+
+                "\t CREDITS: \n\n" +
                 "\t Kakashi Hatake - Robin Kamo\n\n" +
                 "\t Campus Mölndal Who made this possible\n\n" +
-                "\t Every cool person who has shared music online\n\n" +
+                "\t Every cool person who has shared 8bit music\n\n" +
                 "\t Youtube fabvl – Kakashi Rap Song Athousan.\n\n" +
                 "\t Youtube suferas - Naruto - Sadness and Sorrow 8 Bit\n\n" +
                 "\t Youtube Otaku Bits - Naruto Shippuden opening 3 - Blue Bird (8bit)\n\n" +
                 "\t Youtube Musikage - Naruto Opening 2 - Haruka Kanata 8-bit NES Remix\n\n" +
-                "\t Youtube 8 Bit Music Worl - Naruto - The Raising Fighting Spirit (8 bit)\n\n" +
+                "\t Youtube 8 Bit Music Worl - Naruto - The Raising Fighting Spirit (8 bit)\n\n\n" +
                 "\t CREATORS\n" +
                 "\t Daudi - David Ström \n" +
                 "\t Hocke - Håkan Eriksson"
-            };
+                };
             return stories;
         }
     }
