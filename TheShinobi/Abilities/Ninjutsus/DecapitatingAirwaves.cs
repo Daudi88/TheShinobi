f9486030a15ff034733f4115916aef3698861574
Håkan Eriksson <badfish.paint@gmail.com>
2021-01-29 14:10:32 +0100

    Lade till ett par Ninjutsu klasser.

---
+++ TheShinobi/Abilities/Ninjutsus/DecapitatingAirwaves.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Abilities.Ninjutsus
+{
+    class DecapitatingAirwaves : Ninjutsu
+    {
+        public DecapitatingAirwaves()
+        {
+            Name = "Decapitating Airwaves";
+            Damage = "1d8";
+            Cost = 10;
+        }
+    }
+}
---
+++ TheShinobi/Abilities/Ninjutsus/DragonFire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheShinobi.Abilities.Ninjutsus
+{
+    class DragonFire : Ninjutsu
+    {
+        public DragonFire()
+        {
+            Name = "Dragon Fire";
+            Damage = "2d12";
+            Cost = 30;
+        }
+    }
+}
